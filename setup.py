543b252db93e10637a6912a356dfd7ab4dada5f7
Tyler Kennedy <tk@tkte.ch>
2019-02-05 18:21:14 -0500

    Scrub-a-rub-dub, remove bots and deploy scripts.

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
 """
 Notifico is my personal open source MIT replacement to the
 now-defunct http://cia.vc service with my own little spin on things.
@@ -7,35 +5,23 @@ now-defunct http://cia.vc service with my own little spin on things.
 from setuptools import setup, find_packages
 
 
-def get_version():
-    """
-    Load and return the current Notifico version.
-    """
-    local_results = {}
-    execfile('notifico/version.py', {}, local_results)
-    return local_results['__version__']
-
-
 if __name__ == '__main__':
     setup(
         name='Notifico',
-        version=get_version(),
+        version='1.0.0',
         long_description=__doc__,
         packages=find_packages(),
         include_package_data=True,
         zip_safe=False,
         install_requires=[
-            'Flask>=0.12.0',
+            'flask',
             'Flask-WTF==0.8.4',
             'Flask-Gravatar',
             'Flask-SQLAlchemy',
             'Flask-XML-RPC',
             'Flask-Mail',
             'Flask-Caching',
-            'fabric',
             'sqlalchemy',
-            'utopia',
-            'gevent',
             'oauth2',
             'redis',
             'gunicorn',
@@ -44,11 +30,7 @@ if __name__ == '__main__':
             'xmltodict',
             'unidecode',
             'raven',
-            'blinker',
             'docopt',
             'celery'
-        ],
-        dependency_links=[
-            'https://github.com/notifico/utopia/tarball/master#egg=utopia'
         ]
     )
